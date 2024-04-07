fe90c335b48795e3de4fe7e547757dc5f8e93ac6
nakamura0907 <nakamura0907@users.noreply.github.com>
2024-04-07 15:15:54 +0900

    golang: elastic search

--- apps/api/main.go
+++ apps/api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -33,7 +34,47 @@ func main() {
 	fmt.Printf("%+v\n", r)
 
 	e.GET("/", func(c echo.Context) error {
+		var buf bytes.Buffer
+		query := map[string]interface{}{
+			"query": map[string]interface{}{
+				"match": map[string]interface{}{
+					"name": "Brave",
+				},
+			},
+		}
+		if err := json.NewEncoder(&buf).Encode(query); err != nil {
+			return err
+		}
+
+		res, err := client.Search(
+			client.Search.WithContext(c.Request().Context()),
+			client.Search.WithIndex("books"),
+			client.Search.WithBody(&buf),
+			client.Search.WithTrackTotalHits(true),
+			client.Search.WithPretty(),
+		)
+		if err != nil {
+			return err
+		}
+		defer res.Body.Close()
+
+		if res.IsError() {
+			var e map[string]interface{}
+			if err := json.NewDecoder(res.Body).Decode(&e); err != nil {
+				return err
+			}
+			return c.JSONPretty(res.StatusCode, e, "  ")
+		}
+
+		var r map[string]interface{}
+		if err := json.NewDecoder(res.Body).Decode(&r); err != nil {
+			return err
+		}
+
+		fmt.Printf("%+v\n", r)
+
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
